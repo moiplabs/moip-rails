55acf0ed4c0e87230c8fc85ac1fe73e863541212
Herberth Amaral <herberthamaral@gmail.com>
2010-09-01 04:17:03 -0300

    Is there a best message for the first commit?

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MoipRails::Application.config.secret_token = '70f9716fab48ffc58d6ec024e4a0670026b7f976d1700d9c6e0935292b285dc590f57cc55eb85b18800ae5928d6183bdf9bb34f5051442444ab1860b3f9edbd7'
